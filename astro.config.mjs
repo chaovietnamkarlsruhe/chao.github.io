350048ca7fe15aa7bb07b1f88375102bf033a125
chaovietnamkarlsruhe <chaovietnamkarlsruhe@gmail.com>
2024-12-18 23:51:44 +0100

    update repo names

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-    site: 'https://chao-website.github.io',
-    /* todo if necessary:   site: 'https://chaovietnam.de' 'https://chao-website.github.io' 'http://' */
+    site: 'https://chaovietnam.de',
 	integrations: [sitemap()]
 });
