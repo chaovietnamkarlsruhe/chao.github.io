59557fb67f8d623e9a0ccfd005421feef785e86a
chaovietnamkarlsruhe <chaovietnamkarlsruhe@gmail.com>
2024-12-18 20:21:51 +0100

    Base Layer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://chao-website.github.io',
+    /* todo if necessary:   site: 'https://chaovietnam.de' 'https://chao-website.github.io' 'http://' */
+	integrations: [sitemap()]
+});
---
+++ src/consts.ts
@@ -0,0 +1,3 @@
+
+export const SITE_TITLE = 'Chao Vietnam';
+export const SITE_DESCRIPTION = 'Willkommen';
